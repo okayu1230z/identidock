608acd6d0e2f8c35e5266641ad77894eb48761c0
okayu1230z <okamoto.yuki@ist.osaka-u.ac.jp>
2021-11-28 23:11:55 +0900

    first commit

---
+++ app/identidock.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+app = Flask(__name__)
+default_name = 'Joe Bloggs'
+
+@app.route('/')
+def get_identicon():
+    name = default_name
+
+    header = '<html><head><title>Identidock</title></head><body>'
+    body = '''<form method="POST">
+              Hello <input type="text" name="name" value="{}">
+              <input type="submit" value="submit">
+              </form>
+              <p>You look like a:
+              <img src="monster/monster.png"/>
+              '''.format(name)
+    footer = '</body></html>'
+
+    return header + body + footer
+
+@app.route('/monster/<name>')
+def get_identiction(name):
+    r = reguests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+    image = r.content
+
+    return Response(image, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
