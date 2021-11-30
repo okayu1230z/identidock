0747ac8baae76aabc1be7430d269e67b40f0291e
okayu1230z <okamoto.yuki@ist.osaka-u.ac.jp>
2021-12-01 01:43:56 +0900

    minor fix

--- app/identidock.py
+++ app/identidock.py
@@ -1,30 +1,27 @@
 from flask import Flask
-
 app = Flask(__name__)
-default_name = 'Joe Bloggs'
+
+default_name = "Joe Bloggs"
 
 @app.route('/')
 def get_identicon():
+    print('aiueo')
+
     name = default_name
 
-    header = '<html><head><title>Identidock</title></head><body>'
+    header = '<html><head><title>Identidocker</title></head><body>'
     body = '''<form method="POST">
               Hello <input type="text" name="name" value="{}">
               <input type="submit" value="submit">
               </form>
               <p>You look like a:
-              <img src="monster/monster.png"/>
+              <img src="/monster/monster.png"/>
               '''.format(name)
     footer = '</body></html>'
 
     return header + body + footer
 
-@app.route('/monster/<name>')
-def get_identiction(name):
-    r = reguests.get('http://dnmonster:8080/monster/' + name + '?size=80')
-    image = r.content
-
-    return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
